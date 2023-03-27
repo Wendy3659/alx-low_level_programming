1329c1db37e329bfd85bade3616bc21ca9de4fb6
Wendy3659 <esthernjoroge65@gmail.com>
2023-03-26 23:43:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_alphabet - print alphabet in lower case
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
